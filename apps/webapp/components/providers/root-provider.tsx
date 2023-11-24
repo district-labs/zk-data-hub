16535e559f583e0a1e1b00ac639db1e2401ecde8
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2023-11-24 20:21:55 -0300

    refactor: remove siwe to webapp (#7)

--- apps/webapp/components/providers/root-provider.tsx
+++ apps/webapp/components/providers/root-provider.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from "next-themes"
 import { Provider as RWBProvider } from "react-wrap-balancer"
 
 import { useIsMounted } from "@/lib/hooks/use-is-mounted"
-import HandleWalletEvents from "@/components/blockchain/handle-wallet-events"
 import { RainbowKit } from "@/components/providers/rainbow-kit"
 
 const queryClient = new QueryClient()
@@ -25,9 +24,7 @@ export default function RootProvider({ children }: RootProviderProps) {
     >
       <QueryClientProvider client={queryClient}>
         <RWBProvider>
-          <RainbowKit>
-            <HandleWalletEvents>{children}</HandleWalletEvents>
-          </RainbowKit>
+          <RainbowKit>{children}</RainbowKit>
         </RWBProvider>
       </QueryClientProvider>
     </ThemeProvider>
--- apps/webapp/env.mjs
+++ apps/webapp/env.mjs
@@ -3,13 +3,6 @@ import { isAddress } from "viem"
 import { z } from "zod"
 
 export const env = createEnv({
-  server: {
-    // Iron session requires a secret of at least 32 characters
-    NEXTAUTH_SECRET: z
-      .string()
-      .min(32)
-      .default("complex_password_at_least_32_characters_long"),
-  },
   client: {
     NEXT_PUBLIC_PONDER_URL: z.string().url(),
     NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1).optional(),
@@ -19,7 +12,6 @@ export const env = createEnv({
       .refine((value) => isAddress(value)),
   },
   runtimeEnv: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXT_PUBLIC_PONDER_URL: process.env.NEXT_PUBLIC_PONDER_URL,
     NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
